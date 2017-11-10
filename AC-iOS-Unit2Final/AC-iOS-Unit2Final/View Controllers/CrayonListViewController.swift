aff08c0124f20d4dea1c0bc8235a727dba710e54
Melissa He <melissahe@ac.c4q.nyc>
2017-11-10 11:14:07 -0500

    Add working table view

---
+++ AC-iOS-Unit2Final/AC-iOS-Unit2Final/View Controllers/CrayonListViewController.swift
@@ -0,0 +1,86 @@
+//
+//  CrayonListViewController.swift
+//  AC-iOS-Unit2Final
+//
+//  Created by C4Q on 11/10/17.
+//  Copyright © 2017 Karen Fuentes. All rights reserved.
+//
+
+import UIKit
+
+class CrayonListViewController: UIViewController {
+
+    @IBOutlet weak var crayonListTableView: UITableView!
+    
+    //Data Source Variable
+    var allCrayons: [Crayon] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadData()
+        crayonListTableView.delegate = self
+        crayonListTableView.dataSource = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        crayonListTableView.reloadData()
+    }
+    
+    //Setup
+    func loadData() {
+        allCrayons = Crayon.allTheCrayons
+    }
+
+    //MARK: - Navigation
+    
+}
+
+//MARK: - Table View Methods
+extension CrayonListViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    //Delegate Methods
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let selectedCell = tableView.cellForRow(at: indexPath)
+        
+        performSegue(withIdentifier: "detailedSegue", sender: selectedCell)
+    }
+    
+    //Data Source Methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return allCrayons.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "crayonCell", for: indexPath)
+        let currentCrayon = allCrayons[indexPath.row]
+        let currentRed = CGFloat(currentCrayon.red/255)
+        let currentGreen = CGFloat(currentCrayon.green/255)
+        let currentBlue = CGFloat(currentCrayon.blue/255)
+        
+        let oppositeRed = (1 - currentRed) / 1.35
+        let oppositeGreen = (1 - currentGreen) / 1.35
+        let oppositeBlue = (1 - currentBlue) / 1.35
+        
+        if let crayonCell = cell as? CrayonTableViewCell {
+            
+            crayonCell.colorNameLabel.text = currentCrayon.name
+            crayonCell.colorNameLabel.textColor = UIColor(displayP3Red: oppositeBlue, green: oppositeGreen, blue: oppositeRed, alpha: 1)
+            crayonCell.crayonImageView.image = #imageLiteral(resourceName: "transparentCrayon")
+            crayonCell.backgroundColor = UIColor(displayP3Red: currentRed, green: currentGreen, blue: currentBlue, alpha: 1)
+            
+            //if hex
+            crayonCell.decimalHexLabel.text = "Hex: \(currentCrayon.hex)"
+            
+            //if decimal
+//            crayonCell.decimalHexLabel.text = "Decimal: (\(currentCrayon.red), \(currentCrayon.green), \(currentCrayon.blue))"
+            
+            crayonCell.decimalHexLabel.textColor = UIColor(displayP3Red: oppositeBlue, green: oppositeGreen, blue: oppositeRed, alpha: 1)
+            
+            return crayonCell
+        }
+        
+        return cell
+    }
+    
+}
---
+++ AC-iOS-Unit2Final/AC-iOS-Unit2Final/Views/CrayonTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CrayonTableViewCell.swift
+//  AC-iOS-Unit2Final
+//
+//  Created by C4Q on 11/10/17.
+//  Copyright © 2017 Karen Fuentes. All rights reserved.
+//
+
+import UIKit
+
+class CrayonTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var colorNameLabel: UILabel!
+    @IBOutlet weak var crayonImageView: UIImageView!
+    @IBOutlet weak var decimalHexLabel: UILabel!
+    
+}
